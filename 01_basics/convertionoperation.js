dc2ea37688a5627bdcbf483d3f66be3c8bf231a0
Shrestha-Ghosh03 <168276632+Shrestha-Ghosh03@users.noreply.github.com>
2025-03-13 14:23:52 +0000

    convertion on datatypes

--- 01_basics/convertionoperation.js
+++ 01_basics/convertionoperation.js
@@ -1,28 +1,60 @@
-let score = "33abc "
+/*let score = "33abc "
 console.log (typeof score);
-console.log (typeof (score));
+console.log (typeof (score));*/
 
 /*let score1 = "33"
 console.log (typeof score);
 console.log (typeof (score));*/
 
-let valueNumber = Number(score);
+/*let valueNumber = Number(score);
 console.log (typeof valueNumber);
-console.log (valueNumber);
+console.log (valueNumber);*/
 
 // "33" -> 33
 // "33abc" -> Nan
 //"true"-> 1
 
 
-let isloggedin= 1
+/*let isloggedin= 1
 let booleanisloggedIN = Boolean(isloggedin)
-console.log(booleanisloggedIN)
+console.log(booleanisloggedIN)*/
 
 // 1 -> true ; 0-> fAlse 
 // ""-> false
 // "hitesh" -> true
 
-let someNum = 22
+/*let someNum = 22
 let stringNum = String(someNum)
-console.log(typeof stringNum)
+console.log(typeof stringNum)*/
+
+//********************* Operations********************
+
+let value = 3 
+let negValue = -value
+/*console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "hitesh"
+let str3 = str1 + " " +str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1"+ "2");
+console.log("1"+ 2 +2);
+console.log(1+ 2 + "2");
+ 
+console.log(+true);*/
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+//resourses mdn,https://tc39.es/ecma262/multipage/abstract-operations.html
+
+
