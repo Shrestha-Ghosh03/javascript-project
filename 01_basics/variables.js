e6db4220c0386d3ad6f05a7ba07191091889d5b4
Shrestha-Ghosh03 <168276632+Shrestha-Ghosh03@users.noreply.github.com>
2025-03-13 13:49:57 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 55556;
+let accountEmail = "shrestha@gmail.com"
+var accountPassword = "586"
+accountCity = "Delhi"
+let accountState;
+
+//accountId = 56 not allowed
+accountEmail = "dscgdsh.com";
+accountPassword = "659";
+accountCity = "Kolkata"
+/* let used is in block scope and functional scope
+var not tobe used*/
+
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
