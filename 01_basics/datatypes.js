7842b538818ef8c1264fac581106c0a444c45190
Shrestha-Ghosh03 <168276632+Shrestha-Ghosh03@users.noreply.github.com>
2025-03-13 13:43:07 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code s newer version
+
+//alert("hello")  
+// we are using node js not browser
+
+console.log(3+3) // code readability should be high
+
+let name = 'shrestrha' // string 
+let age =1 //number 
+let istrue = false // boolean -> true/ false
+//big int
+//null -> standalone value
+//undefined -> not assigned value
+// symbol -> unique
+
+//object
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
