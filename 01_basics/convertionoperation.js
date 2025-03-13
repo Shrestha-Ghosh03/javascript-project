8a4d9099b3e9e42fbff0c8e2e3599fc913f2235b
Shrestha-Ghosh03 <168276632+Shrestha-Ghosh03@users.noreply.github.com>
2025-03-13 14:05:24 +0000

    converstion of datatypes

---
+++ 01_basics/convertionoperation.js
@@ -0,0 +1,28 @@
+let score = "33abc "
+console.log (typeof score);
+console.log (typeof (score));
+
+/*let score1 = "33"
+console.log (typeof score);
+console.log (typeof (score));*/
+
+let valueNumber = Number(score);
+console.log (typeof valueNumber);
+console.log (valueNumber);
+
+// "33" -> 33
+// "33abc" -> Nan
+//"true"-> 1
+
+
+let isloggedin= 1
+let booleanisloggedIN = Boolean(isloggedin)
+console.log(booleanisloggedIN)
+
+// 1 -> true ; 0-> fAlse 
+// ""-> false
+// "hitesh" -> true
+
+let someNum = 22
+let stringNum = String(someNum)
+console.log(typeof stringNum)
