3e0702b6ce5374f99d0bae1a9ff11b11ddfb2982
Shrestha-Ghosh03 <168276632+Shrestha-Ghosh03@users.noreply.github.com>
2025-03-13 14:51:54 +0000

    datatypesummery

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,16 @@
+// Primitive
+
+ //  7 types : String, NUmber, Boolean, null, Undefined, Symbol,Bigint
+
+// reference type or non-primitive
+
+// Array, Objects, Functions
+
+
+//dynamically  typed language
+
+const id = Symbol('123')
+const id2 = Symbol('123')
+console.log(id === id2); // false
+
+
