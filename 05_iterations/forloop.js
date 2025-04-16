375bc681ab2b9751429c201fa97ee557cfd760f1
Shrestha-Ghosh03 <168276632+Shrestha-Ghosh03@users.noreply.github.com>
2025-04-16 04:31:13 +0000

    forloop

---
+++ 05_iterations/forloop.js
@@ -0,0 +1,14 @@
+// for loop
+
+//syntax 
+/*for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+*/
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
