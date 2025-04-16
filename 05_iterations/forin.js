345293df7535eeb6c6d6bfbf30b2ab71563dcbbc
Shrestha-Ghosh03 <168276632+Shrestha-Ghosh03@users.noreply.github.com>
2025-04-16 06:10:20 +0000

    for-in loop

---
+++ 05_iterations/forin.js
@@ -0,0 +1,24 @@
+const myObject = {
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+/*for (const key in myObject) {
+        const element = myObject[key];
+        console.log(element);
+        
+}*/
+for (const key in myObject) {
+  //  console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+//for-in loops in array => gives the keys / index
+const programming = ["js", "rb","py","java"]
+for (const key in programming) {
+   //console.log(key);
+   
+}
+
+//for-in not for map 
